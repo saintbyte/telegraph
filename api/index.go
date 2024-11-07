23a112aa572fa8585a2b8a6aa3cd5c0de34de7c3
Yegor Kazantsev <wwwadmin@bk.ru>
2024-11-07 23:59:39 +0300

    fararway

--- api/index.go
+++ api/index.go
@@ -3,23 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"runtime"
-	"strings"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		fmt.Fprintf(w, pair[0])
-		fmt.Fprintf(w, "=")
-		fmt.Fprintf(w, pair[1])
-		fmt.Fprintf(w, "<br />")
-	}
-	fmt.Fprintf(w, "GOMAXPROCS")
-	fmt.Fprintf(w, "=")
-	fmt.Fprintf(w, "%v", runtime.NumCPU())
+	fmt.Fprintf(w, "Index")
 	fmt.Fprintf(w, "<br />")
 }
